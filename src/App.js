afd64433b908ec9e197b0696e979cb85aae8bcc1
omarfaruk <omarfarukbd67213@gmail.com>
2021-09-25 19:35:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUu-8erBYIoL-dyHUdleYWrNVjzxCwlSk",
+    authDomain: "react-auth-app-5c35f.firebaseapp.com",
+    projectId: "react-auth-app-5c35f",
+    storageBucket: "react-auth-app-5c35f.appspot.com",
+    messagingSenderId: "324904599977",
+    appId: "1:324904599977:web:0c150e5d07241e9e6f04b8"
+};
+export default firebaseConfig;
